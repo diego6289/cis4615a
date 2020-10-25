2074c0ebf8b6dbfec3f65a48ecb67bec25a48d62
diego6289 <33875932+diego6289@users.noreply.github.com>
2020-10-25 06:57:14 -0400

    Add files via upload

---
+++ R06_MET01_J.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+/*
+    Rule 06. Methods(MET)
+    Met01-J. Never use assertions to validate method arguments.
+
+    This compliant solution validates the method arguments by ensuring that values 
+    passed to Math.abs() exclude Integer.MIN_VALUE and also by checking for integer overflow:
+*/ 
+
+public class R06_MET01_J 
+{
+    public static void main(String[] args) 
+    {
+	     System.out.print(getAbsAdd(-1, 5));
+    }
+    
+    public static int getAbsAdd(int x, int y)
+    {
+      if (x == Integer.MIN_VALUE || y == Integer.MIN_VALUE) {
+        throw new IllegalArgumentException();
+      }
+      int absX = Math.abs(x);
+      int absY = Math.abs(y);
+      if (absX > Integer.MAX_VALUE - absY) {
+        throw new IllegalArgumentException();
+      }
+      return absX + absY;
+    }
+}
