aeedc946f692d0b008b9bca62e6a23d2c1a4b2d7
diego6289 <33875932+diego6289@users.noreply.github.com>
2020-10-25 06:16:05 -0400

    Add files via upload

---
+++ R02_XP00_J.java
@@ -0,0 +1,27 @@
+import java.io.File;
+
+/* 
+	Rule 02. Expressions (EXP)
+	XP00-J. Do not ignore values returned by methods.
+
+	The noncompliant code example attempts to delete a file but fails to 
+	check whether the operation has succeeded:
+
+	This compliant solution checks the Boolean value returned by the delete() 
+	method and handles any resulting errors:
+*/
+
+	public class R02_XP00_J 
+	{
+
+    	public static void main(String[] args) 
+    	{
+      		File someFile = new File("someFileName.txt");
+      		// Do something with someFile
+      		if (!someFile.delete()) 
+      		{
+        		// Handle failure to delete the file
+      		}
+     
+    }
+}
