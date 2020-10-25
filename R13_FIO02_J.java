5629a4318b224b8f1a8b578439a72d5ff76f7509
diego6289 <33875932+diego6289@users.noreply.github.com>
2020-10-25 08:17:24 -0400

    Add files via upload

---
+++ R13_FIO02_J.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.File;
+
+/*
+	Rule 09. Locking (LCK)
+	LCK10-J. Use a correct form of the double-checked locking idiom
+
+	This noncompliant code example attempts to delete a specified file but gives 
+	no indication of its success. The Java platform requires File.delete() 
+	to throw a SecurityException only when the program lacks authorization to 
+	delete the file [API 2014]. No other exceptions are thrown, so the deletion can silently fail.
+
+	Noncompliant Code Example 
+
+	File file = new File(args[0]);
+		file.delete();
+*/
+
+
+public class R13_FIO02_J
+{
+    public static void main(String[] args) 
+    {
+    	File file = new File("file");
+		if (!file.delete())
+  			System.out.println("Deletion failed");
+    }
+}
